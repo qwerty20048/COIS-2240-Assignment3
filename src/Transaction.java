1757a8d02c5da076b1adb91926d54ce30c701fc1
qwerty20048 <65600740+qwerty20048@users.noreply.github.com>
2024-12-04 20:33:22 -0500

    Refactored Transaction class to implement Singleton pattern and made borrowBook and returnBook non-static.

--- src/Transaction.java
+++ src/Transaction.java
@@ -2,14 +2,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Transaction {
+    private static Transaction instance; // Singleton instance
 
-    // Perform the borrowing of a book
-    public static boolean borrowBook(Book book, Member member) {
+    private Transaction() {} // Private constructor
+
+    public static Transaction getTransaction() { // Getter for Singleton instance
+        if (instance == null) {
+            instance = new Transaction();
+        }
+        return instance;
+    }
+
+    public boolean borrowBook(Book book, Member member) {
         if (book.isAvailable()) {
             book.borrowBook();
-            member.borrowBook(book); 
-            String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
-            System.out.println(transactionDetails);
+            member.borrowBook(book);
+            System.out.println(member.getName() + " borrowed " + book.getTitle());
             return true;
         } else {
             System.out.println("The book is not available.");
@@ -17,20 +25,17 @@ public class Transaction {
         }
     }
 
-    // Perform the returning of a book
-    public static void returnBook(Book book, Member member) {
+    public void returnBook(Book book, Member member) {
         if (member.getBorrowedBooks().contains(book)) {
             member.returnBook(book);
             book.returnBook();
-            String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
-            System.out.println(transactionDetails);
+            System.out.println(member.getName() + " returned " + book.getTitle());
         } else {
             System.out.println("This book was not borrowed by the member.");
         }
     }
 
-    // Get the current date and time in a readable format
-    private static String getCurrentDateTime() {
+    private String getCurrentDateTime() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sdf.format(new Date());
     }
